cf5e260cfc4dba3e0e726de65230964c0b14cda3
ahm3dhany <>
2017-01-21 01:47:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# broken-web-application 
+Spring Web-App that contains five different flaws -from the OWASP 2013 Top 10 List- and their fixes.
---
+++ src/main/java/broken/BrokenWebApplication.java
@@ -0,0 +1,12 @@
+package broken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokenWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokenWebApplication.class, args);
+	}
+}
