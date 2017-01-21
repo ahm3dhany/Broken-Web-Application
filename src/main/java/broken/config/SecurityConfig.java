84141aba216d26bea142f07288993a23b518ae43
ahm3dhany <>
2017-01-21 22:47:10 +0300

    Added Missing Function Level Access Control vulnerability (A7)

---
+++ src/main/java/broken/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package broken.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/**
+ *
+ * @author ahm3dhany
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("test").roles("ADMIN");
+        
+    }
+    
+}
